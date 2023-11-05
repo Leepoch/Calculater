70ca9766327bb6fe5aff4d42512af3ce3ef17d3e
Leepoch <daniiltinkovv@gmail.com>
2023-11-06 00:34:56 +0300

    finishCalculater

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputExpression = scanner.nextLine().split("");
+        ExpressionInfo returnValue = new ExpressionInfo();
+        String mathSymbol = returnValue.getMathSymbol(inputExpression);
+        String firstNumber = returnValue.getOperators(inputExpression, mathSymbol)[0].trim();
+        String secondNumber = returnValue.getOperators(inputExpression, mathSymbol)[1].trim();
+        String[] operators = returnValue.getOperators(inputExpression, mathSymbol);
+        if(mathSymbol.isEmpty() || operators.length > 2) {
+            throw new IOException("Выражение не удовлетворяет условиям");
+        }
+        String firstNumberType = returnValue.getNumberType(firstNumber);
+        String secondNumberType = returnValue.getNumberType(firstNumber);
+
+        if (firstNumberType.equals("arabicNumber") && secondNumberType.equals("arabicNumber")) {
+            System.out.println(returnValue.getResultInArabicNumber(firstNumber, secondNumber, mathSymbol));
+        } else if(firstNumberType.equals("romeNumber") && secondNumberType.equals("romeNumber")) {
+            System.out.println(returnValue.getResultInRomeNumber(firstNumber, secondNumber, mathSymbol));
+        } else {
+            throw new IOException("Операнды разных типов");
+        }
+    }
+}
+
+class ExpressionInfo {
+    String getResultInRomeNumber(String firstNumber,String secondNumber,String mathSymbol) throws IOException {
+        String[] romeNumbers = {"I","II","III","Iv","V","VI","VII","VIII","IX","X"};
+        DigitConverter firstRomeNumber = DigitConverter.valueOf(firstNumber);
+        DigitConverter secondRomeNumber = DigitConverter.valueOf(secondNumber);
+        String firstArabicNumber = firstRomeNumber.getConversion();
+        String secondArabicNumber = secondRomeNumber.getConversion();
+        int result = getResultInArabicNumber(firstArabicNumber, secondArabicNumber, mathSymbol);
+        if(result < 1 || result > 10) {
+            throw new IOException("Итоговое выражение меньше единицы или больше десяти");
+        }
+        return romeNumbers[result-1];
+    }
+
+    int getResultInArabicNumber(String firstNumber, String secondNumber, String mathSymbol) throws IOException {
+      int intFirstNumber = Integer.parseInt(firstNumber);
+      int intSecondNumber = Integer.parseInt(secondNumber);
+      if(intFirstNumber < 1 || intFirstNumber > 10) throw new IOException("Первое число не удовлетворяет условиям");
+      if(intSecondNumber < 1 || intSecondNumber > 10) throw new IOException("Второе число не удовлетворяет условиям");
+      switch (mathSymbol) {
+          case "-":
+              return intFirstNumber - intSecondNumber;
+          case "/":
+              return intFirstNumber / intSecondNumber;
+          case "\\+":
+              return intFirstNumber + intSecondNumber;
+          case "\\*":
+              return intFirstNumber * intSecondNumber;
+      }
+      throw new IOException("Выражение не удовлетворяет условиям");
+    }
+
+    String getNumberType(String expressionNumber) {
+        String[] arabicNumbers = {"1","2","3","4","5","6","7","8","9","10"};
+        String numbersType = "";
+        try {
+            for(String arabicNumber:arabicNumbers) {
+                DigitConverter romeNumber = DigitConverter.valueOf(expressionNumber);
+                String romeToArabicNumber = romeNumber.getConversion();
+                if(arabicNumber.equals(romeToArabicNumber)) {
+                    numbersType = "romeNumber";
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            for(String arabicNumber:arabicNumbers){
+                if(expressionNumber.equals(arabicNumber)) {
+                    return "arabicNumber";
+                }
+            }
+            throw new IllegalArgumentException("Выражение не удовлетворяет условиям");
+        }
+        return numbersType;
+    }
+
+    String[] getOperators(String[] input, String mathSymbol) {
+        String newInput = String.join("", input);
+        return newInput.split(mathSymbol);
+    }
+
+    String  getMathSymbol(String[] input) {
+        for(String item:input) {
+            if(item.equals("+")) return "\\+";
+            if(item.equals("*")) return "\\*";
+            if(item.equals("-")) return "-";
+            if(item.equals("/")) return "/";
+        }
+        return "";
+    }
+}
